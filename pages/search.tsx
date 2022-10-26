d474d78551aa8b767bbacf9bee198cdf4de9e268
bpa03 <baldassarepugliese@gmail.com>
2022-10-26 17:24:43 -0400

    Agregado filtro para buscar character por status

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,38 +1,84 @@
 import { NextPage } from 'next';
 import { useState, ChangeEvent, FormEvent } from 'react';
 import CharacterList from '../components/CharacterList';
-import { Character } from '../services/interfaces';
+import { CharactersResponse } from '../services/interfaces';
 import RickAndMortyApi from '../services/RickAndMortyApi';
 
-const Search: NextPage = () => {
-  const [search, setSearch] = useState<string>('');
-  const [characters, setCharacters] = useState<Character[]>([]);
+interface FormValues {
+  name: string;
+  status: 'alive' | 'dead' | 'unknown' | '';
+}
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  }
+const Search: NextPage = () => {
+  const [characters, setCharacters] = useState<CharactersResponse | null>(
+    null
+  );
+  const [values, setValues] = useState<FormValues>({
+    name: '',
+    status: '',
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    console.log({ name, value });
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    RickAndMortyApi.searchCharacters(search).then((data) => {
-      setCharacters(data.results);
+
+    let format = '';
+    const { name, status } = values;
+
+    if (name !== '') {
+      format += `name=${name}`;
+    }
+
+    if (status !== '') {
+      format += `&status=${status}`;
+    }
+
+    RickAndMortyApi.searchCharacters(format).then((data) => {
+      if (data.results) {
+        setCharacters(data);
+      }
     });
-  }
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
+          name="name"
           className="border border-black"
           onChange={handleChange}
         />
-        <button>
-          Search
-        </button>
+        <select name="status" onChange={handleChange}>
+          <option value="">Select a status</option>
+          <option value="alive">Alive</option>
+          <option value="dead">Dead</option>
+          <option value="unknown">Unknown</option>
+        </select>
+        <button>Search</button>
       </form>
+      {characters && (
+        <div className="mt-6">
+          <div>
+            <h3>Count: {characters.info.count}</h3>
+            <h3>Pages: {characters.info.pages}</h3>
+          </div>
+        </div>
+      )}
       <div className="mt-6">
-        <CharacterList characters={characters} />
+        {characters && characters.results ? (
+          <CharacterList characters={characters?.results} />
+        ) : null}
       </div>
     </div>
   );
