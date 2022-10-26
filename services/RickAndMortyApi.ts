97010603e80a6e63e7fa2ea76bc054bd474eb7dd
bpa03 <baldassarepugliese@gmail.com>
2022-10-26 16:26:03 -0400

    Servicios para obtener location y buscar character por nombre

--- services/RickAndMortyApi.ts
+++ services/RickAndMortyApi.ts
@@ -1,20 +1,46 @@
-import { AllCharactersResponse } from './interfaces';
+import { CharactersResponse, LocationResponse } from './interfaces';
 
 class RickAndMortyApi {
   declare apiUrl: string;
   declare charantersUri: string;
-  
+  declare locationUri: string;
+
   constructor() {
-    this.apiUrl = "https://rickandmortyapi.com/api";
-    this.charantersUri = "character";
+    this.apiUrl = 'https://rickandmortyapi.com/api';
+    this.charantersUri = 'character';
+    this.locationUri = 'location';
   }
 
-  async getAllCharacters(): Promise<AllCharactersResponse> {
-    const formattedUrl = `${this.apiUrl}/${this.charantersUri}`
+  async getAllCharacters(): Promise<CharactersResponse> {
+    const formattedUrl = `${this.apiUrl}/${this.charantersUri}`;
     const response = await fetch(formattedUrl);
     const data = await response.json();
-    
-    return data as AllCharactersResponse;
+
+    return data as CharactersResponse;
+  }
+
+  async getLocationById(id: string | number) {
+    const formattedUrl = `${this.apiUrl}/${this.locationUri}/${id}`;
+    const response = await fetch(formattedUrl);
+    const data = await response.json();
+
+    return data as LocationResponse;
+  }
+
+  async searchCharacters(search: string) {
+    const formattedUrl = `${this.apiUrl}/${this.charantersUri}?name=${search}`;
+    const response = await fetch(formattedUrl);
+    const data = await response.json();
+
+    return data as CharactersResponse;
+  }
+
+  async getCharactersFromPage(page: number = 1) {
+    const formattedUrl = `${this.apiUrl}/${this.charantersUri}?page=${page}`;
+    const response = await fetch(formattedUrl);
+    const data = await response.json();
+
+    return data as CharactersResponse;
   }
 }
 
